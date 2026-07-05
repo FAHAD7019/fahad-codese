49e8ddcdc9b6c4da5537bd8973da192deb39062b
FAHAD7019 <hfhd9899@gmail.com>
2026-07-05 07:06:20 +0300

    Create commands-data.js

---
+++ assets/js/commands-data.js
@@ -0,0 +1,4 @@
+const commands = [
+    { name: "gamemode", category: "Gamemode", syntax: "/gamemode <mode>", description: "تغيير نمط اللعب", example: "/gamemode creative" },
+    { name: "tp", category: "Teleport", syntax: "/tp <x> <y> <z>", description: "الانتقال", example: "/tp 100 64 100" }
+];
